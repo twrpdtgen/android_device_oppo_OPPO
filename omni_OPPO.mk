5dfd4a065bd2e0ec1c5f9db81329227d3ab82f0b
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-08-21 11:26:03 +0200

    OPPO: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: oppo Model: None Script version: 3.0.0

---
+++ omni_OPPO.mk
@@ -0,0 +1,27 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from OPPO device
+$(call inherit-product, device/oppo/OPPO/device.mk)
+
+PRODUCT_DEVICE := OPPO
+PRODUCT_NAME := omni_OPPO
+PRODUCT_MANUFACTURER := oppo
+
+PRODUCT_GMS_CLIENTID_BASE := android-oppo
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="full_oppo6765-user 10 QP1A.190711.020 bedd37e98646d3a1 release-keys"
+
+BUILD_FINGERPRINT := None
